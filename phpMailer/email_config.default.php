1fdf9c8d9b8fa85d335404fca0f9ede31f98631d
valor323 <caine101@mail.chapman.edu>
2018-10-24 16:40:26 -0700

    initial setup of portfolio

---
+++ phpMailer/email_config.default.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','<YOUR EMAIL>');
+define('EMAIL_USERNAME', '<Name>');
+define('EMAIL_PASS','<YOUR PASSWORD>');
+define('EMAIL_TO_ADDRESS', '<EMAIL_TO_SEND_TO>');
+?>
---
+++ phpMailer/email_config.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','whatevered0323@gmail.com');
+define('EMAIL_USERNAME', 'Chase Caine');
+define('EMAIL_PASS','Holydiver@1');
+define('EMAIL_TO_ADDRESS', 'caine101@mail.chapman.edu');
+?>
